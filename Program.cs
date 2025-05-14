ba06e484ab71aa83167f0beaca8b3f9e0df5642d
artemgon <artemhoncharuk123@gmail.com>
2025-05-14 18:46:25 +0300

    Task 7 is completed.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace project_26_05_25
+﻿using System;
+using System.Threading;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace project_26_05_25
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter base number: ");
+            double baseNum = double.Parse(Console.ReadLine() ?? "0");
+            Console.Write("Enter exponent: ");
+            double exponent = double.Parse(Console.ReadLine() ?? "0");
+
+            double result = await CalculatePowerAsync(baseNum, exponent);
+            Console.WriteLine($"{baseNum}^{exponent} = {result}");
+        }
+
+        static async Task<double> CalculatePowerAsync(double b, double e)
+        {
+            return await Task.Run(() => Math.Pow(b, e));
         }
     }
 }
