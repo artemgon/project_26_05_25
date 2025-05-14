dab98240d331ac5743d4cdf34865884b6c7d2b49
artemgon <artemhoncharuk123@gmail.com>
2025-05-14 18:38:28 +0300

    Task 1 is created.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,50 @@
-﻿namespace project_26_05_25
+﻿using System;
+using System.Threading;
+
+namespace project_26_05_25
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Thread numThread = new (GenerateNumbers) { Priority = ThreadPriority.Normal };
+            Thread letterThread = new(GenerateLetters) { Priority = ThreadPriority.AboveNormal };
+            Thread symbThread = new(GenerateSymbols) { Priority = ThreadPriority.BelowNormal };
+
+            numThread.Start();
+            letterThread.Start();
+            symbThread.Start();
+
+            numThread.Join();
+            letterThread.Join();
+            symbThread.Join();
+        }
+
+        static void GenerateNumbers()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(100);
+            }
+        }
+
+        static void GenerateLetters()
+        {
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                Console.WriteLine(c);
+                Thread.Sleep(100);
+            }
+        }
+
+        static void GenerateSymbols()
+        {
+            for (char c = '!'; c <= '/'; c++)
+            {
+                Console.WriteLine(c);
+                Thread.Sleep(100);
+            }
         }
     }
 }
